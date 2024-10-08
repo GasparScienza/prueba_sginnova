4869f1ae659ddc2eaee4794846460a20a5f0f8dd
Gaspar Scienza <gasparscienza@hotmail.com>
2024-10-08 11:46:05 -0300

    Comienzo de proyecto

---
+++ src/main/java/com/gasparscienza/prueba_sginnova/PruebaSginnovaApplication.java
@@ -0,0 +1,13 @@
+package com.gasparscienza.prueba_sginnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSginnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSginnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasparscienza/prueba_sginnova/PruebaSginnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasparscienza.prueba_sginnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSginnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
